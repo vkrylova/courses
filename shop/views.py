ca676e437e932db74cc3fbcb5d25c9602e43970f
vkrylova <veronika0krylova@gmail.com>
2025-11-01 00:14:15 +0300

    create models Category,Course and register them in Admin panel. Render course titles on web page

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Course
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello from the Shop app")
+    courses = Course.objects.all()
+    return HttpResponse(''.join([f"{str(course)}<br>" for course in courses]))
