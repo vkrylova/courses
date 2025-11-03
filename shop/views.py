92a3e996ce19dc94bb921f500ef0121010ffc120
vkrylova <veronika0krylova@gmail.com>
2025-11-03 16:42:37 +0300

    add single_course template and configure routing
    Reorganized templates folder structure for better clarity.
    Linked courses and single_course pages via URL patterns.

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Category, Course
+from django.shortcuts import render, get_object_or_404
+from .models import Course
 
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', context={'courses': courses})
+    return render(request, template_name='shop/courses.html', context={'courses': courses})
+
+
+def single_course(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, template_name='shop/single_course.html', context={'course': course})
