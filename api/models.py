1cde11e1c946b466b6370e833776bfd9ddfc5f4f
vkrylova <veronika0krylova@gmail.com>
2025-11-04 21:54:10 +0300

    add models for api and routing

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
 
-# Create your models here.
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
