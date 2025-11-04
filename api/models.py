1701efa2f5b67d469d5e9d90a94a4388466055f2
vkrylova <veronika0krylova@gmail.com>
2025-11-04 22:07:49 +0300

    configure authentication and authorization for api

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from tastypie.resources import ModelResource
 from shop.models import Category, Course
+from tastypie.authorization import Authorization
+from .authentication import CustomAuthentication
 
 
 class CategoryResource(ModelResource):
@@ -14,3 +16,5 @@ class CourseResource(ModelResource):
         queryset = Course.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
+        authentication = CustomAuthentication()
+        authorization = Authorization()
